66d7cff8b97d7a124238e97978b5168074d6b559
at4tec <at4tec@gmail.com>
2022-01-28 20:45:24 +0200

    no-const-One-return-Only-two-users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
-}
+const profiles =[
+  {
+    id: 1,
+    userID: '1',
+    favoriteMovieID: '2',
+  },
+  {
+    id: 2,
+    userID: '2',
+    favoriteMovieID: '1',
+  },
+];
+
+const users = [
+  {},
+   {
+    id: 1,
+    name: 'Jane Cruz',
+    userName: 'coder',
+  },
+   {
+    id: 2,
+    name: 'Matthew Johnson',
+    userName: 'mpage',
+  },
+  ]
+
+const movies = {
+  1: {
+    id: 1,
+    name: 'Planet Earth 1',
+  },
+  2: {
+    id: 2,
+    name: 'Selma',
+  },
+  };
+
+
+function App () {
+   return (
+       <ul>  
+	<h2>Favorite Movies</h2>
+          {/*
+          For each object in the profiles array, make a list of JSX elements.
+          The map function returns a new array, which will
+          only have list elements in it. React will automatically know how to
+          render an array of JSX objects.
+          */}
+         
+           
+          { profiles.map( (e) => ( 
+              <li >
+            
+               {users[e.userID].name}'s favorite movie is "{movies[e.favoriteMovieID].name}".
+              </li>
+
+                
+            )
+           ) 
+          }
+      </ul>
+    );
+  }
+
+
+
+
+export default App;
